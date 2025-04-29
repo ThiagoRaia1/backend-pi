87e6d616386687cb5846e39514cd10c814a18136
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-04-29 12:57:11 -0300

    Pequena alteração

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
    // Habilitar CORS
-   app.enableCors({
-    origin: 'http://localhost:8081', // URL onde o frontend está rodando
-    methods: 'GET, POST, PUT, DELETE', // Métodos permitidos
-    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
-  });
+   app.enableCors()
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
