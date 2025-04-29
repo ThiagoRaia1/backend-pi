cb77ec1861f932caa99b403e16b88c527a970600
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-04-29 08:24:15 -0300

    Adição do CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
+   // Habilitar CORS
+   app.enableCors({
+    origin: 'http://localhost:8081', // URL onde o frontend está rodando
+    methods: 'GET, POST, PUT, DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap()
