33d3325cb97632a8dd94f677254a158b6134b61b
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-06-06 09:53:34 -0300

    Aumenta o limite do bodyParser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Aumenta o limite de payload para 10MB (ajuste conforme necessário)
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
   app.useGlobalPipes(new ValidationPipe());
   // Habilitar CORS
   app.enableCors();
